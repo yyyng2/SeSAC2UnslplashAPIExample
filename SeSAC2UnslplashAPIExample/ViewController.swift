76508bae3445128b6a647bff6169165fc2dbc903
yyyng2 <yyyng2@gmail.com>
2022-10-26 11:59:16 +0900

    Initial Commit

---
+++ SeSAC2UnslplashAPIExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC2UnslplashAPIExample
+//
+//  Created by Y on 2022/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
